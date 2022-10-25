0800e357aca752d07c7925e602e5c7cede1c0d9a
FancyKat <fancykatbase@gmail.com>
2022-10-25 18:11:49 +0000

    Animation Class

---
+++ assets/components/animation.h
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+class Animation
+{  
+    public: 
+        Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime);
+        ~Animation();
+
+        sf::IntRect uvRect;
+
+
+    private:
+        sf::Vector2u imageCount;
+        sf::Vector2u currentImage;
+
+        float totalTime;
+        float switchTime;
+};
